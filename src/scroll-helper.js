4a7da61460f1b49680bebbc2186a5bad7199f6ba
John Bartel <john@johnbartel.us>
2021-12-12 19:34:52 -0600

    Show only the title now when scrolled. - Animate the resizing, instead of snapping instantly, which makes it less confusing what happened. - Main post content can now only take a max of 50% screen height before scrolling.

--- src/scroll-helper.js
+++ src/scroll-helper.js
@@ -1,7 +1,8 @@
 // Hide post content body when scrolling down, and revealing on up
 const SCROLLED_UP = -1;
 const SCROLLED_DOWN = 1;
-const SCROLL_THRESHOLD = 25;
+const SCROLL_UP_THRESHOLD = 400;
+const SCROLL_DOWN_THRESHOLD = 200;
 
 let relativePageY = -1;
 let direction = 0;
@@ -26,11 +27,11 @@ document.addEventListener("scroll", function onScroll(e) {
   }
 
   // Set direction based on passing a threshold, otherwise do nothing
-  if (pageY > relativePageY + SCROLL_THRESHOLD) {
+  if (pageY > relativePageY + SCROLL_DOWN_THRESHOLD) {
     direction = SCROLLED_DOWN;
     relativePageY = pageY;
     onScrollDirectionChange(direction);
-  } else if (pageY < relativePageY - SCROLL_THRESHOLD) {
+  } else if (pageY < relativePageY - SCROLL_UP_THRESHOLD) {
     direction = SCROLLED_UP;
     relativePageY = pageY;
     onScrollDirectionChange(direction);
@@ -41,10 +42,16 @@ let ignoreScroll = false;
 function onScrollDirectionChange(direction) {
   if (ignoreScroll) return;
 
+  let post = document.querySelector(".fatitem");
+
   if (direction === SCROLLED_DOWN) {
     document.body.classList.add("scrolled_down");
+    post.scrollTo(0, 0);
+    let { bottom } = document.querySelector(".title").getBoundingClientRect();
+    post.style.maxHeight = bottom + 5 + "px";
   } else {
     document.body.classList.remove("scrolled_down");
+    post.style.maxHeight = "50vh";
   }
 
   // Ignore scroll events until after repaints since that causes some
